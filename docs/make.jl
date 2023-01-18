687b349be6512611012bd93628689cb7b1ca732c
Daniel St <daniel.steinmaurer@htl-braunau.at>
2023-01-18 22:20:11 +0100

    add deploy

--- docs/make.jl
+++ docs/make.jl
@@ -10,8 +10,9 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "https://github.com/Dan00712/Hexdump.jl",
-    target = "build",
-    branch = "gh-pages"
-)=#
+deploydocs(
+    repo = "github.com/Dan00712/Hexdump.jl",
+    devbranch = "main",
+    deps=nothing,
+    make=nothing
+)
